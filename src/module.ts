de9a77982cced354feceebe2fd2fb2710ca4d547
Jan-Markus Langer <janmarkuslanger10121994@gmail.com>
2022-12-16 20:44:13 +0100

    Rename meta data of module

--- src/module.ts
+++ src/module.ts
@@ -7,8 +7,8 @@ export interface ModuleOptions {
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'my-module',
-    configKey: 'myModule'
+    name: 'nuxt-contentful-module',
+    configKey: 'nuxtContentfulModule'
   },
   defaults: {
     addPlugin: true
